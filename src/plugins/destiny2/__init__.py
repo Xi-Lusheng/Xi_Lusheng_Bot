8004fca5147d78d4ab90ad6d2a77580b81e6b69d
汐鹿生 <l1983791415@126.com>
2022-08-05 19:56:32 +0800

    first add

---
+++ src/plugins/destiny2/__init__.py
@@ -0,0 +1,37 @@
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.params import CommandArg
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot, Message
+
+import urllib3
+import json
+
+today = on_command("today", aliases={'日报', }, priority=5)
+
+
+@today.handle()
+async def handle_first_receive(event: GroupMessageEvent, message: Message = CommandArg()):
+    try:
+        try:
+            url = 'http://www.tianque.top/d2api/today/'
+
+            r = urllib3.PoolManager().request('GET', url)
+            hjson = json.loads(r.data.decode())
+
+            img_url = hjson["img_url"]
+
+            cq = "[CQ:image,file=" + img_url + ",id=40000]"
+
+            await today.send(Message(cq))
+        except:
+            url = 'http://www.tianque.top/d2api/today/'
+
+            r = urllib3.PoolManager().request('GET', url)
+            hjson = json.loads(r.data.decode())
+
+            error_url = hjson["error"]
+            await today.send("获取日报失败\n" +
+                             "error:\n" +
+                             error_url)
+    except:
+        await today.send("获取日报失败:\n服务器错误")
