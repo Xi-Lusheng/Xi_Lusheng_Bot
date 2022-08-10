bcb5afd129c7a217e55a0a3e037278185a2f13a4
汐鹿生 <l1983791415@126.com>
2022-08-10 14:45:02 +0800

    新增樱花插件第一次未找到，将执行模糊搜索进行第二次查找

--- src/plugins/reptile/__init__.py
+++ src/plugins/reptile/__init__.py
@@ -1,3 +1,5 @@
+import time
+
 from nonebot.adapters.onebot.v11 import Message, MessageSegment, Bot, MessageEvent
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg, ArgPlainText
@@ -20,12 +22,24 @@ async def sakura_(matcher: Matcher, args: Message = CommandArg()):
 @sakura.got('name', prompt='你想看什么番呢？')
 async def get_sakura_comic(bot: Bot, event: MessageEvent, name: str = ArgPlainText('name')):
     results = (await get_sakura(str(name)))[:repeater_group]
+    await sakura.send("开始查找.....请不要进行其它操作")
     msg = []
     for result in results:
-        msg.append(Message(MessageSegment.text(result['name']) + '\n' +
-                           MessageSegment.image(result['img']) + '\n' +
-                           MessageSegment.text(result['url'])))
+        msg.append(MessageSegment.text(result['name']) + '\n' +
+                   MessageSegment.image(result['img']) + '\n' +
+                   MessageSegment.text(result['url']))
     try:
-        await send_forward_msg_group(bot, event, name="初号姬", msgs=msg if msg else ["没有在樱花找到这个动漫呢，换个姿势搜索试试"])
+        if msg:
+            await send_forward_msg_group(bot, event, name="初号姬", msgs=msg)
+        else:
+            time.sleep(1)
+            await sakura.send('第一次查找失败，将进行模糊查找')
+            results = (await get_sakura(str(name[:2])))[:repeater_group]
+            msg = []
+            for result in results:
+                msg.append(MessageSegment.text(result['name']) + '\n' +
+                           MessageSegment.image(result['img']) + '\n' +
+                           MessageSegment.text(result['url']))
+            await send_forward_msg_group(bot, event, name="初号姬", msgs=msg if msg else ["没有在樱花找到这个动漫呢，换词姿势搜索试试"])
     except:
         await sakura.send('找番插件错误，请尽快联系汐鹿生修复')
