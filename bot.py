d2d6fdbadf7abbe94e0f679cb9a20684bed35802
汐鹿生 <l1983791415@126.com>
2022-08-21 18:43:06 +0800

    添加抽象语句功能

--- bot.py
+++ bot.py
@@ -4,16 +4,6 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter
 
-# Custom your logger
-# 
-# from nonebot.log import logger, default_format
-# logger.add("error.log",
-#            rotation="00:00",
-#            diagnose=False,
-#            level="ERROR",
-#            format=default_format)
-
-# You can pass some keyword args config to init function
 nonebot.init()
 app = nonebot.get_asgi()
 
@@ -21,15 +11,7 @@ driver = nonebot.get_driver()
 
 driver.register_adapter(Adapter)
 
-
-# Please DO NOT modify this file unless you know what you are doing!
-# As an alternative, you should use command `nb` or modify `pyproject.toml` to load plugins
 nonebot.load_from_toml("pyproject.toml")
-
-# Modify some config / config depends on loaded configs
-# 
-# config = driver.config
-# do something...
 
 
 if __name__ == "__main__":
