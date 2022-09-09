aaac89a21cbd89d2ce607040a1fe6291ae7987bf
汐鹿生 <l1983791415@126.com>
2022-09-09 19:27:53 +0800

    提高触发表情包概率

--- src/plugins/emoticon/__init__.py
+++ src/plugins/emoticon/__init__.py
@@ -10,7 +10,7 @@ emo = on_message(priority=96, block=False)
 
 @emo.handle()
 async def emo_(event: MessageEvent):
-    if random.randint(1, 10) >= 7:
+    if random.randint(1, 10) >= 3:
         msg = str(event.get_message())
         msg = re.sub(r"\[.*?\]", "", msg)
         emo_path = await get_emo(msg)
