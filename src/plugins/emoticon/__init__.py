1be8a6e251f594ad1693bde40c2ddc384700c88c
汐鹿生 <l1983791415@126.com>
2022-09-14 19:11:02 +0800

    取消概率发表情包

--- src/plugins/emoticon/__init__.py
+++ src/plugins/emoticon/__init__.py
@@ -10,14 +10,11 @@ emo = on_message(priority=96, block=False)
 
 @emo.handle()
 async def emo_(event: MessageEvent):
-    if random.randint(1, 10) >= 3:
-        msg = str(event.get_message())
-        msg = re.sub(r"\[.*?\]", "", msg)
-        emo_path = await get_emo(msg)
-        if emo_path:
-            await emo.send(emo_path)
-        else:
-            pass
+    msg = str(event.get_message())
+    msg = re.sub(r"\[.*?\]", "", msg)
+    emo_path = await get_emo(msg)
+    if emo_path:
+        await emo.send(emo_path)
     else:
         pass
 
