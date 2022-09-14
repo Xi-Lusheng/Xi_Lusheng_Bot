8f1671a01d634c4485a24beba4feba7122b25e4a
汐鹿生 <l1983791415@126.com>
2022-09-14 18:20:32 +0800

    删除原思知api的配置

--- utils/config.py
+++ utils/config.py
@@ -28,13 +28,3 @@ if 'over_list' not in config.keys():
     over_list = config.get('over_list', 20)
 else:
     over_list = config.get('over_list')
-
-if 'sizhi_id' not in config.keys():
-    logger.warning('[初号姬] 未发现配置项 `SIZHI_ID` , 请在env中配置')
-else:
-    app_id = config.get('sizhi_id')
-
-if 'sizhi_user_id' not in config.keys():
-    logger.warning('[初号姬] 未发现配置项 `SIZHI_USER_ID` , 请在env中配置')
-else:
-    user_id = config.get('sizhi_user_id')
