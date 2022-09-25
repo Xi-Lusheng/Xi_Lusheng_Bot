31c0689a4678caea0055e0c704a39335ff6c5a45
汐鹿生 <l1983791415@126.com>
2022-09-25 15:01:09 +0800

    添加全体禁言指令

--- src/plugins/admin/__init__.py
+++ src/plugins/admin/__init__.py
@@ -1,6 +1,9 @@
 from nonebot import on_regex
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
 import re
+
+from nonebot.permission import SUPERUSER
+
 from utils.config import Bot_NICKNAME, Bot_ID, COMMAND_START
 from utils.permission import admin_permission
 from nonebot.plugin import PluginMetadata
@@ -14,7 +17,7 @@ __plugin_meta__ = PluginMetadata(
             {
                 'func': '撤回消息',
                 'trigger_method': 'on_re',
-                'trigger_condition': '[点击回复选择需要撤回的信息] /撤回',
+                'trigger_condition': '[回复选择需要撤回的信息] /撤回',
                 'brief_des': '撤回消息',
                 'detail_des': '撤回消息'
             },
@@ -49,18 +52,18 @@ async def withdraw_(bot: Bot, event: GroupMessageEvent):
         else:
             await withdraw.finish(f'{Bot_NICKNAME}没有足够权限哦，让群主大大给{Bot_NICKNAME}个管理员权限吧')
 
-# taboo_all = on_regex(f'(^{COMMAND_START}全体禁言$|^{COMMAND_START}关闭全体禁言$)', priority=5, block=True, permission=SUPERUSER)
-#
-#
-# @taboo_all.handle()
-# async def taboo_all_(bot: Bot, event: GroupMessageEvent):
-#     if await admin_permission(bot, event):
-#         group_id = event.group_id
-#         bot_role = (await bot.get_group_member_info(
-#             group_id=group_id,
-#             user_id=Bot_ID,
-#             no_cache=False))['role']
-#
-#         await bot.set_group_whole_ban(group_id=int(event.group_id), enable=bool(True))
-#     else:
-#         await withdraw.finish('你没有权限使用这个命令哦', at_sender=True)
+taboo_all = on_regex(f'(^{COMMAND_START}全体禁言$|^{COMMAND_START}关闭全体禁言$)', priority=5, block=True, permission=SUPERUSER)
+
+
+@taboo_all.handle()
+async def taboo_all_(bot: Bot, event: GroupMessageEvent):
+    print(event.get_plaintext())
+    if await admin_permission(bot, event):
+        enable = None
+        if event.get_plaintext() == '/全体禁言':
+            enable = True
+        elif event.get_plaintext() == '/关闭全体禁言':
+            enable = False
+        await bot.set_group_whole_ban(group_id=int(event.group_id), enable=bool(enable))
+    else:
+        await withdraw.finish('你没有权限使用这个命令哦', at_sender=True)
