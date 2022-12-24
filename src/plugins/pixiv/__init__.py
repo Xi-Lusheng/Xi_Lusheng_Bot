666af4a39139317cc0c4582c3d054bb9dd1e9306
汐鹿生 <l1983791415@126.com>
2022-12-24 19:10:30 +0800

    修改r18幻影坦克进入正常图片发送阶段

--- src/plugins/pixiv/__init__.py
+++ src/plugins/pixiv/__init__.py
@@ -53,40 +53,41 @@ async def pixiv_(event: Event):
     msg = event.get_plaintext()
     r18 = re.search('r18', msg)
     r16 = re.search('(涩图|色图)', msg)
+    data = None
     if r18:
         await pixiv.send('请等待合成幻影坦克', at_sender=True)
         data = await get_image(sort=2)
     elif r16:
         await pixiv.send('请等待合成幻影坦克', at_sender=True)
         data = await get_image(sort=1)
-        try:
-            message = await make_new_image(data)
-            image = MessageSegment.image(f"base64://{base64.b64encode(message['new_image'].getvalue()).decode()}")
-            result = image + MessageSegment.text(f"\n图片id：{message['image_id']}\n"
-                                                 f"图片链接：{message['image_url']}")
-            await pixiv.finish(result)
-        except (ActionFailed, ValueError):
-            await pixiv.send("合成失败将只发送原图链接")
-            await pixiv.finish(MessageSegment.text(f"图片id：{data['image_id']}\n"
-                                                   f"图片链接：{data['image_url']}"))
-    else:
-        data = await get_image(sort=0)
     try:
-        image = MessageSegment.image(data['image_url'])
-        result = image + MessageSegment.text(f"\n图片id：{data['image_id']}\n"
-                                             f"图片链接：{data['image_url']}")
-        await pixiv.finish(result)
-    except ActionFailed:
-        await pixiv.send("图片发送失败可能被风控，将合成幻影坦克")
         message = await make_new_image(data)
         image = MessageSegment.image(f"base64://{base64.b64encode(message['new_image'].getvalue()).decode()}")
         result = image + MessageSegment.text(f"\n图片id：{message['image_id']}\n"
                                              f"图片链接：{message['image_url']}")
         await pixiv.finish(result)
-    except ValueError:
-        await pixiv.send("幻影坦克合成失败，将发送图片原链接")
+    except (ActionFailed, ValueError):
+        await pixiv.send("合成失败将只发送原图链接")
         await pixiv.finish(MessageSegment.text(f"图片id：{data['image_id']}\n"
                                                f"图片链接：{data['image_url']}"))
+    else:
+        data = await get_image(sort=0)
+        try:
+            image = MessageSegment.image(data['image_url'])
+            result = image + MessageSegment.text(f"\n图片id：{data['image_id']}\n"
+                                                 f"图片链接：{data['image_url']}")
+            await pixiv.finish(result)
+        except ActionFailed:
+            await pixiv.send("图片发送失败可能被风控，将合成幻影坦克")
+            message = await make_new_image(data)
+            image = MessageSegment.image(f"base64://{base64.b64encode(message['new_image'].getvalue()).decode()}")
+            result = image + MessageSegment.text(f"\n图片id：{message['image_id']}\n"
+                                                 f"图片链接：{message['image_url']}")
+            await pixiv.finish(result)
+        except ValueError:
+            await pixiv.send("幻影坦克合成失败，将发送图片原链接")
+            await pixiv.finish(MessageSegment.text(f"图片id：{data['image_id']}\n"
+                                                   f"图片链接：{data['image_url']}"))
 
 
 delete_image = on_command("删除图片", block=True, priority=10, permission=SUPERUSER)
